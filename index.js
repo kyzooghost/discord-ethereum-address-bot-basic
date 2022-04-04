d66c785caa40a3e06efee8b25518107016aa7805
kyzooghost <jeffersonzjtang@gmail.com>
2022-04-04 19:43:05 +1000

    removed /view-all slash command

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const { Client, Collection, Intents } = require('discord.js');
 require('dotenv').config()
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// Hash map that contains mapping of Discord handle to Ethereum address
-const discordEthereumMap = new Map();
-
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
@@ -24,24 +21,5 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(client, ...args));
 	}
 }
-
-// client.once('ready', () => {
-// 	console.log('Ready!');
-// });
-
-// client.on('interactionCreate', async interaction => {
-// 	if (!interaction.isCommand()) return;
-
-// 	const command = client.commands.get(interaction.commandName);
-
-// 	if (!command) return;
-
-// 	try {
-// 		await command.execute(interaction);
-// 	} catch (error) {
-// 		console.error(error);
-// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-// 	}
-// });
 
 client.login(process.env.TOKEN);
