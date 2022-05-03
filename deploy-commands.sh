e97659354cf4d6ad01eb0dce48cf196b9323b527
kyzooghost <jeffersonzjtang@gmail.com>
2022-05-03 17:09:59 +1000

    added deploy-commands.sh

---
+++ deploy-commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. env.sh
+
+curl -X POST \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bot $TOKEN" \
+-d '{"name":"connect","description":"Connect your Ethereum address","options":[{"name":"ethereum-address","description":"Your Ethereum address","type":3,"required":true}]}' \
+"https://discord.com/api/v8/applications/$CLIENT_ID/commands"
+
+curl -X POST \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bot $TOKEN" \
+-d '{"name":"delete","description":"Delete your connected Ethereum address"}' \
+"https://discord.com/api/v8/applications/$CLIENT_ID/commands"
+
+curl -X POST \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bot $TOKEN" \
+-d '{"name":"view","description":"Connect your Ethereum address"}' \
+"https://discord.com/api/v8/applications/$CLIENT_ID/commands"
